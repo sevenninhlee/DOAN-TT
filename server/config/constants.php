0ac413a59a361b000a7bc78a2f7cb0587e5fe5af
NinhLe <lephuocninh7118@gmail.com>
2019-11-25 15:11:02 +0700

    Fix active  account and layout

--- server/config/constants.php
+++ server/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'FRONTEND_URL' => env('CLIENT_URL', 'https://localhost:8080')
+  'FRONTEND_URL' => env('CLIENT_URL', 'https://localhost:8080/#')
     // 'FRONTEND_URL' => [
     //     'PROD' => env('CLIENT_URL', 'https://localhost:8080/'),
     //     'DEV' => env('CLIENT_URL', 'https://localhost:8080/')
